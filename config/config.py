5284e18d663f774b993c19f12438ab7e811805f7
official-royal <127479359+official-royal@users.noreply.github.com>
2023-04-08 17:40:04 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,14 +27,14 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrtosumemon/tosuXMusic",
+    "https://github.com/mrtosumemon/tosuxmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/mrtosumemon")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Aboutprincexd")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/INCRICIBLE")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OFFICIAL_ROYAL_NETWORK")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ROYAL_DISSCUS")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
